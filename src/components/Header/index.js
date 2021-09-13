5530cd7d9506ba5cd38b04305ad013860b182f0b
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-13 20:42:40 +0600

    added register

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,10 +10,11 @@ const Header = () => {
         <Navbar expand="lg" variant="light" bg="primary" fixed="top" className="d-flex justify-content-between p-3 mb-5">
             <div>
                 <Link to="/">
-                    <FontAwesomeIcon icon={faBookReader} className="text-primary fs-1 text-black"/>
+                    <FontAwesomeIcon icon={faBookReader} className=" header-icon text-primary fs-1 text-black"/>
                 </Link>
             </div>
             <div>
+                <Link to="/" className="me-3 text-decoration-none text-black fs-4"> Homepage</Link>
                 <Link to="/news" className="me-3 text-decoration-none text-black fs-4"> News </Link>
                 <Link to="/auth" className="me-3 text-decoration-none text-black fs-4"> Sign in </Link>
                 <Link to="/register" className="me-3 text-decoration-none text-black fs-4"> Register </Link>
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import Layout from "../Layout";
+import {Button, Col, Form, Row} from "react-bootstrap";
 
 const Register = () => {
     return (
-        <div>
+        <Layout>
+            <Row className="mt-5">
+                <Col md={9}>
+                    <Form className="offset-5">
+                        <h2 className="align-items-center">Register</h2>
+                        <Form.Group className="mb-3 " controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control   type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-        </div>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Layout>
     );
 };
 
