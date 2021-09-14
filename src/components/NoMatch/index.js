216aa16dc6993efa363aef25e5c01e28339a55f1
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-14 13:25:12 +0600

    deleted useLocation

--- src/components/NoMatch/index.js
+++ src/components/NoMatch/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {useLocation} from "react-router-dom"
+
 import Layout from "../Layout";
 
 const NoMatch = () => {
 
 
-    let location = useLocation();
+
     return (
         <Layout>
             <h3 className="text-center fs-2 mt-4">
-                No match for <code>{location.pathname}</code>
+                No match for
             </h3>
 
         </Layout>
