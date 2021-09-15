78fc34b8b3eecf25469f4a9fdbbb8b3c85df494d
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-15 15:53:52 +0600

    added transition

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,23 +5,23 @@ import {Link, NavLink} from "react-router-dom";
 import "./header.css"
 import {Button, Form} from "react-bootstrap";
 import axios from "axios";
-
+import {CSSTransition} from 'react-transition-group';
 
 const Header = () => {
     const initial = {name: "", email: "", phone: ""}
     const [isOpen, setIsOpen] = useState(false)
-    const [callback, setCallback] =useState(initial)
+    const [callback, setCallback] = useState(initial)
     const [sentSuccess, setSentSuccess] = useState(false)
 
     const handleInput = (e) => {
-       setCallback({...callback, [e.target.name]: e.target.value})
+        setCallback({...callback, [e.target.name]: e.target.value})
     }
-    const handleSubmit =(e) => {
+    const handleSubmit = (e) => {
         e.preventDefault()
         axios.post("https://6115f1038f38520017a3863c.mockapi.io/callback", callback)
-            .then(()=>{
+            .then(() => {
                 setSentSuccess(true)
-                setTimeout(()=>{
+                setTimeout(() => {
                     setIsOpen(false)
                     setSentSuccess(false)
                     setCallback(initial)
@@ -32,38 +32,46 @@ const Header = () => {
 
     return (
         <>
-        <header className="header">
-            <div className=" d-flex justify-content-between p-3 mb-5">
-                <div>
-                    <Link to="/" className="header-link    text-decoration-none">
-                        <FontAwesomeIcon icon={faBookReader} className=" header-icon text-primary fs-1 text-black"/>
-                        <span className="text-black fs-5 fw-bold"> USA Today</span>
-                    </Link>
+            <header className="header">
+                <div className=" d-flex justify-content-between p-3 mb-5">
+                    <div>
+                        <Link to="/" className="header-link    text-decoration-none">
+                            <FontAwesomeIcon icon={faBookReader} className=" header-icon text-primary fs-1 text-black"/>
+                            <span className="text-black fs-5 fw-bold"> USA Today</span>
+                        </Link>
+                    </div>
+                    <div>
+                        <NavLink to="/" exact
+                                 className=" header-link  me-3 text-decoration-none  fs-4 "> Homepage</NavLink>
+                        <NavLink to="/news"
+                                 className=" header-link     me-3 text-decoration-none  fs-4"> News </NavLink>
+                        <NavLink to="/auth" className=" header-link     me-3 text-decoration-none  fs-4"> Sign
+                            in </NavLink>
+                        <NavLink to="/register"
+                                 className=" header-link      me-3 text-decoration-none  fs-4"> Register </NavLink>
+                        <button type="button" onClick={() => setIsOpen(true)} className="btn btn-success"> Submit an
+                            application
+                        </button>
+                    </div>
                 </div>
-                <div>
-                    <NavLink to="/" exact className=" header-link  me-3 text-decoration-none  fs-4 "> Homepage</NavLink>
-                    <NavLink to="/news" className=" header-link     me-3 text-decoration-none  fs-4"> News </NavLink>
-                    <NavLink to="/auth" className=" header-link     me-3 text-decoration-none  fs-4"> Sign in </NavLink>
-                    <NavLink to="/register" className=" header-link      me-3 text-decoration-none  fs-4"> Register </NavLink>
-                    <button type="button" onClick={()=> setIsOpen(true)} className="btn btn-success"> Submit an application</button>
-                </div>
-            </div>
-        </header>
+            </header>
 
-            {
-                isOpen &&  <div className="modal-window">
-                        <div className="modal-cont">
-
-                            {
-                                sentSuccess ? <h4>"Successfully sent"</h4> : <Form className="" onSubmit={handleSubmit}>
+            <CSSTransition in={isOpen} timeout={200}  unmountOnExit     classNames="my-node">
+                <div className="modal-window">
+                    <div className="modal-cont">
+                        {
+                            sentSuccess ? <h4>"Successfully sent"</h4> :
+                                <Form className="" onSubmit={handleSubmit}>
                                     <Form.Group className="mb-3 " controlId="formBasicName">
                                         <Form.Label htmlFor="name">Name</Form.Label>
-                                        <Form.Control id="name"  type="text" placeholder="Enter name" name="name" required  value={callback.name} onChange={handleInput}/>
+                                        <Form.Control id="name" type="text" placeholder="Enter name" name="name"
+                                                      required value={callback.name} onChange={handleInput}/>
                                     </Form.Group>
 
                                     <Form.Group className="mb-3 " controlId="formBasicEmail">
                                         <Form.Label htmlFor="email">Email address</Form.Label>
-                                        <Form.Control  id="email" name="email" type="email" placeholder="Enter email"  required value={callback.email} onChange={handleInput} />
+                                        <Form.Control id="email" name="email" type="email" placeholder="Enter email"
+                                                      required value={callback.email} onChange={handleInput}/>
                                         <Form.Text className="text-muted">
                                             We'll never share your email with anyone else.
                                         </Form.Text>
@@ -71,23 +79,22 @@ const Header = () => {
 
                                     <Form.Group className="mb-3" controlId="formBasicPhone">
                                         <Form.Label htmlFor="phone">Phone number</Form.Label>
-                                        <Form.Control id="phone" name="phone"  type="text" placeholder="phone number"   required value={callback.phone} onChange={handleInput} />
+                                        <Form.Control id="phone" name="phone" type="text" placeholder="phone number"
+                                                      required value={callback.phone} onChange={handleInput}/>
                                     </Form.Group>
                                     <Button variant="primary" type="submit" className="mb-4">
                                         Submit
                                     </Button>
                                 </Form>
-                            }
-
-
-                        <button className="close-modal mt-2" onClick={()=> {
+                        }
+                        <button className="close-modal mt-2" onClick={() => {
                             setIsOpen(false)
-                            setSentSuccess((false))}
-                        }>✖</button>
+                            setSentSuccess((false))
+                        }}>✖</button>
                     </div>
                 </div>
-            }
-            </>
+            </CSSTransition>
+        </>
     );
 };
 
