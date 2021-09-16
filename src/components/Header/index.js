ed89b93d6b82ab69231ec0b5265a61907d9f298b
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-16 11:40:11 +0600

    added modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -29,7 +29,6 @@ const Header = () => {
             })
     }
 
-
     return (
         <>
             <header className="header">
--- src/views/Students/index.js
+++ src/views/Students/index.js
@@ -4,14 +4,33 @@ import axios from "axios";
 import { Container} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
+import Modal from 'react-modal';
 
 const Students = () => {
     const [students, setStudents] = useState([])
+    const [modalIsOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         axios("https://6115f1038f38520017a3863c.mockapi.io/students")
             .then(({data}) => setStudents(data))
     })
+    function openModal() {
+        setIsOpen(true);
+    }
+    function closeModal() {
+        setIsOpen(false);
+    }
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+        },
+    };
+
 
     return (
         <Layout>
@@ -53,8 +72,26 @@ const Students = () => {
     </tr>
     )}
 </tbody>
-        <button className="btn btn-success my-3 d-block ms-auto"  onClick={}>Add contact</button>
+        <button className="btn btn-success my-3 d-block ms-auto" onClick={()=>setIsOpen(true)} >Add contact</button>
     </table>
+
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="Example Modal"
+                >
+                    <button onClick={closeModal}>close</button>
+                    <div>I am a modal</div>
+                    <form>
+                        <input />
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                    </form>
+                </Modal>
+
             </Container>
         </Layout>
     );
