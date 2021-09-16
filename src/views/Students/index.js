7e8a634d2657082c8db418654c30fd5fefac7ccc
Begimai Agybaeva <begimai.a@yandex.ru>
2021-09-16 12:25:04 +0600

    added react hook

--- src/views/Students/index.js
+++ src/views/Students/index.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import Layout from "../../components/Layout";
 import axios from "axios";
-import { Container} from "react-bootstrap";
+import {Col, Container, Form, Row} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {faEdit, faTrash, faWindowClose} from "@fortawesome/free-solid-svg-icons";
 import Modal from 'react-modal';
+import { useForm } from 'react-hook-form';
 
 const Students = () => {
     const [students, setStudents] = useState([])
@@ -20,16 +21,6 @@ const Students = () => {
     function closeModal() {
         setIsOpen(false);
     }
-    const customStyles = {
-        content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-        },
-    };
 
 
     return (
@@ -74,24 +65,73 @@ const Students = () => {
 </tbody>
         <button className="btn btn-success my-3 d-block ms-auto" onClick={()=>setIsOpen(true)} >Add contact</button>
     </table>
-
                 <Modal
                     isOpen={modalIsOpen}
                     onRequestClose={closeModal}
-                    style={customStyles}
                     contentLabel="Example Modal"
                 >
-                    <button onClick={closeModal}>close</button>
-                    <div>I am a modal</div>
-                    <form>
-                        <input />
-                        <button>tab navigation</button>
-                        <button>stays</button>
-                        <button>inside</button>
-                        <button>the modal</button>
-                    </form>
-                </Modal>
+                    <h3>Fill the form</h3>
+                        <Form>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} controlId="formGridName">
+                                    <Form.Label htmlFor="name">Name</Form.Label>
+                                    <Form.Control  id="name" name="name"   type="text" placeholder="Enter name" />
+                                </Form.Group>
 
+                                <Form.Group as={Col} controlId="formGridPhone">
+                                    <Form.Label htmlFor="phone">Phone</Form.Label>
+                                    <Form.Control  id="phone" name="phone" type="text" placeholder="Phone" />
+                                </Form.Group>
+                            </Row>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} controlId="formGridContract">
+                                    <Form.Label htmlFor="contract">Contract</Form.Label>
+                                    <Form.Control id="contract" name="contract"  type="text" placeholder="Sum" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridPaid">
+                                    <Form.Label htmlFor="paid">Paid</Form.Label>
+                                    <Form.Control  id="paid"  name="paid"  type="text" placeholder="Amount" />
+                                </Form.Group>
+                            </Row>
+
+                            <Form.Group className="mb-3" controlId="formGridLaptop">
+                                <Form.Label htmlFor="laptop">Laptop</Form.Label>
+                                <Form.Control  id="laptop" name="laptop" placeholder="Need or not" />
+                            </Form.Group>
+
+                            <Row className="mb-3">
+                                <Form.Group as={Col} controlId="formGridGroup">
+                                    <Form.Label>Group</Form.Label>
+                                    <Form.Select defaultValue="Choose...">
+                                        <option>Choose...</option>
+                                        <option>Morning</option>
+                                        <option>Evening</option>
+                                    </Form.Select>
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridStatus">
+                                    <Form.Label>Status</Form.Label>
+                                    <Form.Select defaultValue="Choose...">
+                                        <option>Choose...</option>
+                                        <option>Active</option>
+                                        <option>Finished</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId="formGridGender">
+                                    <Form.Label>Gender</Form.Label>
+                                    <Form.Select defaultValue="Choose...">
+                                        <option>Choose...</option>
+                                        <option>Male</option>
+                                        <option>Female</option>
+                                    </Form.Select>
+                                </Form.Group>
+                            </Row>
+
+                            <button onClick={closeModal} className="btn btn-sm"><FontAwesomeIcon icon={faWindowClose}/></button>
+                            <button className="btn btn-outline-success">Add </button>
+                        </Form>
+                </Modal>
             </Container>
         </Layout>
     );
